1d09c0ff7b2f9fd10023a5bf60d8ffc2b39bde86
Jason Lin <jlin116@ucsc.edu>
2018-01-31 15:13:04 -0800

    Arrow indicator with different color

--- Assets/Scripts/ArrowIndicators.cs
+++ Assets/Scripts/ArrowIndicators.cs
@@ -3,11 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
+/*
+ * Author: Jason Lin
+ * 
+ * Description:
+ * 3D arrow indicator manager to handle multiple arrow pointing at the given player's position
+ * Methods to add/remove arrow
+ */
+
 public class ArrowIndicators : NetworkBehaviour
 {
+    /// <summary>
+    /// Arrow Indicator GameObject
+    /// </summary>
     public GameObject arrowIndicator;
-    // Dictionary of <player unique Network ID, Arrow Object>
-    Dictionary<int, GameObject> arrowList;
+
+    /// <summary>
+    /// Dictioanry to keep track of which connection id is connecting with which player object
+    /// </summary>
+    private Dictionary<int, GameObject> arrowList;
+
+    /// <summary>
+    /// Array of tracker scheme to be placed based on number of player
+    /// </summary>
+    private TrackerScheme[] trackerScheme = { TrackerScheme.Blue, TrackerScheme.Red, TrackerScheme.Green };
 
     private void Awake()
     {
@@ -26,6 +45,7 @@ public class ArrowIndicators : NetworkBehaviour
 
         GameObject trackPlayer = Instantiate(arrowIndicator, this.transform);
         trackPlayer.GetComponent<TrackPlayer>().objectToTrack = playerObject;
+        trackPlayer.GetComponent<TrackPlayer>().ChangeScheme(trackerScheme[arrowList.Count + 1]);
         arrowList.Add(uniqueId, trackPlayer);
     }
 
--- Assets/Scripts/TrackPlayer.cs
+++ Assets/Scripts/TrackPlayer.cs
@@ -2,12 +2,61 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * Author: Jason Lin
+ * 
+ * Description:
+ * Tracking GameObject provided by the caller, points towards the object
+ */
+
+/// <summary>
+/// Enum defines different tracker scheme options available
+/// </summary>
+public enum TrackerScheme
+{
+    Red,
+    Blue,
+    Green
+};
+
 public class TrackPlayer : MonoBehaviour
 {
+    /// <summary>
+    /// Game Object to track with
+    /// </summary>
     public GameObject objectToTrack;
+
+    /// <summary>
+    /// Changes the arrow to predetermined scheme option
+    /// </summary>
+    /// <param name="scheme"></param>
+    public void ChangeScheme(TrackerScheme scheme)
+    {
+        Material mat = transform.Find("Arrow_Test").GetComponent<MeshRenderer>().material;
+
+        if (mat)
+        {
+            // Changes the arrow texture
+            switch (scheme)
+            {
+                case TrackerScheme.Blue:
+                    mat.color = Color.blue;
+                    break;
+                case TrackerScheme.Red:
+                    mat.color = Color.red;
+                    break;
+                case TrackerScheme.Green:
+                    mat.color = Color.green;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
 
     private void Update()
     {
+        // Start tracking if target is provided
         if (objectToTrack)
         {
             transform.LookAt(objectToTrack.transform);
