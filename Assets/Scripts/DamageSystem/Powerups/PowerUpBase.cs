b3aa10b2407f43e75587ce63677b9eb202dedd25
Jason Lin <jlin116@ucsc.edu>
2018-02-14 16:35:15 -0800

    Create a base class for the powerup control

---
+++ Assets/Scripts/DamageSystem/Powerups/PowerUpBase.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpBase : MonoBehaviour
+{
+    protected virtual void OnPress()
+    {
+
+    }
+
+    protected virtual void OnHold()
+    {
+
+    }
+
+    protected virtual void OnRelease()
+    {
+
+    }
+
+    private void Update()
+    {
+        // Get input from player
+        if (Input.GetButtonDown("Controller_Button_Y"))
+        {
+            OnPress();
+        }
+        else if (Input.GetButtonUp("Controller_Button_Y"))
+        {
+            OnRelease();
+        }
+        else if (Input.GetButton("Controller_Button_Y"))
+        {
+            OnHold();
+        }
+    }
+}
