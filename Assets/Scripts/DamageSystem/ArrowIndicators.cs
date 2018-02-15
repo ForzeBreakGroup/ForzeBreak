7a31dd735aaf0233751e98c83e34ea68e71ef266
Jason Lin <jlin116@ucsc.edu>
2018-02-14 17:33:11 -0800

    Adding comments to new scripts wrote

--- Assets/Scripts/DamageSystem/ArrowIndicators.cs
+++ Assets/Scripts/DamageSystem/ArrowIndicators.cs
@@ -13,16 +13,28 @@ using Photon;
 
 public class ArrowIndicators : Photon.MonoBehaviour
 {
-    /// <summary>
-    /// Arrow Indicator GameObject
-    /// </summary>
-    public GameObject arrowIndicator;
+    #region Private Members
 
     /// <summary>
     /// Dictioanry to keep track of which connection id is connecting to which arrow
     /// </summary>
     private Dictionary<int, GameObject> arrowList;
 
+    #endregion
+
+    #region Public Members
+
+    /// <summary>
+    /// Arrow Indicator GameObject
+    /// </summary>
+    public GameObject arrowIndicator;
+
+    #endregion
+
+    #region Private Methods
+    /// <summary>
+    /// Initializes ArrowList dictionary
+    /// </summary>
     private void Awake()
     {
         if (!arrowIndicator)
@@ -32,30 +44,46 @@ public class ArrowIndicators : Photon.MonoBehaviour
         arrowList = new Dictionary<int, GameObject>();
     }
 
+    /// <summary>
+    /// Start listening to photon events
+    /// </summary>
     private void OnEnable()
     {
         PhotonNetwork.OnEventCall += EvtAddPlayerToMatchHandler;
         PhotonNetwork.OnEventCall += EvtRemovePlayerFromMatchHandler;
     }
 
+    /// <summary>
+    /// Stops listening to photon events
+    /// </summary>
     private void OnDisable()
     {
         PhotonNetwork.OnEventCall -= EvtAddPlayerToMatchHandler;
         PhotonNetwork.OnEventCall -= EvtRemovePlayerFromMatchHandler;
     }
 
+    /// <summary>
+    /// Photon Event handler for OnPlayerSpawnFinished
+    /// Loop through the scene to find all player objects and start tracking the new player object
+    /// </summary>
+    /// <param name="evtCode"></param>
+    /// <param name="content"></param>
+    /// <param name="senderid"></param>
     private void EvtAddPlayerToMatchHandler(byte evtCode, object content, int senderid)
     {
         if (evtCode == (byte)ENetworkEventCode.OnPlayerSpawnFinished && photonView.isMine)
         {
             // Loop through all objects in game to make sure all players are included
             NetworkPlayerData[] playersInGame = FindObjectsOfType<NetworkPlayerData>();
-            foreach(NetworkPlayerData p in playersInGame)
+            foreach (NetworkPlayerData p in playersInGame)
             {
+                // If the photonView is not myself, means it's other player
                 if (!p.photonView.isMine)
                 {
+                    // If the dictionary record does not have the photonView ID start tracking it
                     if (!arrowList.ContainsKey(p.photonView.ownerId))
                     {
+                        // Create an arrow for this specific remote client
                         GameObject arrow = Instantiate(arrowIndicator, this.transform);
                         arrow.GetComponent<TrackPlayer>().AssignTarget(p.gameObject);
                         arrowList.Add(p.photonView.ownerId, arrow);
@@ -65,6 +93,13 @@ public class ArrowIndicators : Photon.MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Photon Event handler for OnRemovePLayerFromMatch
+    /// Use the player left the match to remove the arrow corresponding to it
+    /// </summary>
+    /// <param name="evtCode"></param>
+    /// <param name="content"></param>
+    /// <param name="senderid"></param>
     private void EvtRemovePlayerFromMatchHandler(byte evtCode, object content, int senderid)
     {
         if (evtCode == (byte)ENetworkEventCode.OnRemovePlayerFromMatch && photonView.isMine)
@@ -78,4 +113,6 @@ public class ArrowIndicators : Photon.MonoBehaviour
             }
         }
     }
+
+    #endregion
 }
--- Assets/Scripts/DamageSystem/ReticleSystem.cs
+++ Assets/Scripts/DamageSystem/ReticleSystem.cs
@@ -2,26 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * Author: Jason Lin
+ * 
+ * Description:
+ * Arrow 3D tracker will switch to Reticle view if the target is within the camera.
+ */
 public class ReticleSystem : MonoBehaviour
 {
+    #region Private Members
+    /// <summary>
+    /// Boolean flag indicate if the ReticleSystem for this arrow is enabled
+    /// </summary>
     private bool reticleEnable = false;
+
+    /// <summary>
+    /// Camera reference
+    /// </summary>
     private Camera cam;
+
+    /// <summary>
+    /// The collider to obtain the bound from
+    /// </summary>
     private Collider objectCollider;
 
+    /// <summary>
+    /// The Target this arrow is pointing at
+    /// </summary>
     private GameObject target;
 
-    public void EnableReticleSystem(GameObject target)
-    {
-        if (!target)
-        {
-            Debug.LogError("Target Cannot be Null");
-        }
-        this.target = target;
-        cam = NetworkManager.playerCamera;
-        objectCollider = target.transform.Find("Collider").GetComponentInChildren<Collider>();
-        reticleEnable = true;
-    }
+    #endregion
 
+    #region Private Methods
+
+    /// <summary>
+    /// Every physics update, this script calcualtes if target's collision bound is within camera's viewport
+    /// If it is, it sends a raycast to make sure there's nothing in between
+    /// Then switches to reticle mode when all conditions are satisfied
+    /// </summary>
     private void FixedUpdate()
     {
         if (reticleEnable)
@@ -43,4 +61,25 @@ public class ReticleSystem : MonoBehaviour
             }
         }
     }
+
+    #endregion
+
+    #region Public Methods
+    /// <summary>
+    /// Explicit call to enable the reticle system
+    /// </summary>
+    /// <param name="target"></param>
+    public void EnableReticleSystem(GameObject target)
+    {
+        if (!target)
+        {
+            Debug.LogError("Target Cannot be Null");
+        }
+        this.target = target;
+        cam = NetworkManager.playerCamera;
+        objectCollider = target.transform.Find("Collider").GetComponentInChildren<Collider>();
+        reticleEnable = true;
+    }
+
+    #endregion
 }
