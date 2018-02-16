0db21e715bca66183cac5657ac47be96db10d6b1
Robin_BoyangZhou <z2367570158@gmail.com>
2018-02-16 14:21:06 -0800

    auto destroy of bullets

--- Assets/Scripts/DamageSystem/Powerups/MachineGun&Cannon/BulletMovement.cs
+++ Assets/Scripts/DamageSystem/Powerups/MachineGun&Cannon/BulletMovement.cs
@@ -8,37 +8,31 @@ public class BulletMovement : NetworkPowerUpMovement
     public float velocity = 100f;
     public float explosionRadius = 2.0f;
     public float explosionForce = 1000f;
-    
+    public float existingTime = 3f;
+
+    private float spawnTime = 0f;
     protected override void Awake()
     {
         base.Awake();
+        rb.velocity = velocity * transform.forward;
+        spawnTime = Time.time;
     }
     
     protected override void Move()
     {
         base.Move();
 
-        rb.velocity = velocity * transform.forward;
-
+        if(Time.time>spawnTime+existingTime)
+        {
+            PhotonNetwork.Destroy(gameObject);
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
 
         PhotonNetwork.Instantiate("Explosion1", transform.position, Quaternion.identity,0);
-        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
         PhotonNetwork.Destroy(gameObject);
-
-        
-        //foreach (Collider c in colliders)
-        //{
-        //    Rigidbody r = c.transform.root.GetComponent<Rigidbody>();
-        //    if (r != null)
-        //    {
-        //        r.AddExplosionForce(explosionForce, transform.position, explosionRadius);
-        //    }
-        //}
-
 
     }
 }
