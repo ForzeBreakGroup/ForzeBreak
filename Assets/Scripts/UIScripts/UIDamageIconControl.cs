e83f19212d8e5ed19db3843009f0a78b0535f83c
Robin_BoyangZhou <z2367570158@gmail.com>
2018-02-23 12:56:25 -0800

    damage UI

--- Assets/Scripts/UIScripts/UIDamageIconControl.cs
+++ Assets/Scripts/UIScripts/UIDamageIconControl.cs
@@ -4,17 +4,43 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class UIDamageIconControl : MonoBehaviour {
-    
 
 
+    [SerializeField]
+    private Image displayedIcon;
+    [SerializeField]
+    private Sprite[] resources;
+
+    private DamageSystem damageSystem;
     // Use this for initialization
     void Awake()
     {
+        displayedIcon = GetComponent<Image>();
+
+        damageSystem = NetworkManager.localPlayer.GetComponent<DamageSystem>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(damageSystem.damageAmplifyPercentage<200f)
+        {
+            displayedIcon.sprite = resources[0];
+        }
+        else if(damageSystem.damageAmplifyPercentage < 300f)
+        {
+            displayedIcon.sprite = resources[1];
 
+        }
+        else if (damageSystem.damageAmplifyPercentage < 400f)
+        {
+            displayedIcon.sprite = resources[2];
+
+        }
+        else if (damageSystem.damageAmplifyPercentage < 500f)
+        {
+
+            displayedIcon.sprite = resources[3];
+        }
     }
 }
