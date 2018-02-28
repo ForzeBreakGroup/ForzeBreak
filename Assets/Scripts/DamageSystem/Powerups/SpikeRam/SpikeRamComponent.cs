0e22536bfa87309607b5c726b7f5879d963b71db
Jason Lin <jlin116@ucsc.edu>
2018-02-28 15:30:50 -0800

    SpikeRam with explosion damage and effect

--- Assets/Scripts/DamageSystem/Powerups/SpikeRam/SpikeRamComponent.cs
+++ Assets/Scripts/DamageSystem/Powerups/SpikeRam/SpikeRamComponent.cs
@@ -7,18 +7,24 @@ public class SpikeRamComponent : PowerUpBase
     [Range(0, 50000)]
     [SerializeField] private int damage = 10000;
 
+    [Range(1, 4)]
+    [SerializeField]
+    private int capacity = 2;
+
     public override void AdjustModel()
     {
         transform.localPosition = componentOffset;
         transform.localRotation = Quaternion.identity;
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
-        if (collision.transform.root.tag == "Player")
+        PhotonView otherPhotonView = other.transform.root.gameObject.GetPhotonView();
+        if (other.transform.root.tag == "Player")
         {
-            PhotonView otherPhotonView = collision.transform.root.gameObject.GetPhotonView();
-            otherPhotonView.RPC("CreateExplosion", PhotonPlayer.Find(otherPhotonView.viewID), damage, collision.contacts[0].point, 3.0f);
+            Debug.Log("Spike RAM!");
+            otherPhotonView.RPC("CreateExplosion", PhotonPlayer.Find(otherPhotonView.viewID), (float)damage, transform.position, 30.0f);
+            --capacity;
         }
     }
 }
