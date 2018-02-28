2e34a875e3a1005039ee6e9a6a32a91083f13c33
Jason Lin <jlin116@ucsc.edu>
2018-02-27 17:15:42 -0800

    Fix multiple damage taken when hit by explosion

--- Assets/Scripts/DamageSystem/Powerups/Missile/MissileExplosionEffect.cs
+++ Assets/Scripts/DamageSystem/Powerups/Missile/MissileExplosionEffect.cs
@@ -10,9 +10,9 @@ public class MissileExplosionEffect : Photon.MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.transform.root.tag == "Player")
+        PhotonView otherPhotonView = other.transform.root.gameObject.GetPhotonView();
+        if (other.transform.root.tag == "Player" && otherPhotonView.isMine)
         {
-            PhotonView otherPhotonView = other.transform.root.gameObject.GetPhotonView();
             Vector3 impactCenter = transform.position;
             otherPhotonView.RPC("CreateExplosion", PhotonPlayer.Find(otherPhotonView.viewID), ExplosionForce, transform.position, ExplosionRadius);
         }
