54c08a432d2977f713003a99ccfc9898567ad462
Jason Lin <jlin116@ucsc.edu>
2018-03-02 15:01:32 -0800

    Reduce explosion effect duration

--- Assets/Scripts/DamageSystem/Powerups/Missile/MissileExplosionEffect.cs
+++ Assets/Scripts/DamageSystem/Powerups/Missile/MissileExplosionEffect.cs
@@ -7,15 +7,28 @@ public class MissileExplosionEffect : Photon.MonoBehaviour
 {
     [SerializeField] private float ExplosionForce = 2500;
     [SerializeField] private float ExplosionRadius = 30;
+    [SerializeField] private float ExplosionDuration = 0.2f;
+
+    private bool stillInEffect = false;
+    private float elapsedTime = 0f;
 
     private void OnTriggerEnter(Collider other)
     {
         //PhotonView otherPhotonView = other.transform.root.gameObject.GetPhotonView();
-        if (other.transform.root.tag == "Player")
+        if (other.transform.root.tag == "Player" && !stillInEffect)
         {
             Vector3 impactCenter = transform.position;
             other.transform.root.GetComponent<DamageSystem>().CreateExplosion(ExplosionForce, transform.position, ExplosionRadius);
             //otherPhotonView.RPC("CreateExplosion", PhotonPlayer.Find(otherPhotonView.viewID), ExplosionForce, transform.position, ExplosionRadius);
+        }
+    }
+
+    private void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        if (elapsedTime >= ExplosionDuration)
+        {
+            stillInEffect = true;
         }
     }
 }
