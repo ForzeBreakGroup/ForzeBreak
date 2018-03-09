14c81bd4f0fcfe01b451a9cdb9b9e1e15c8fc455
Jason Lin <jlin116@ucsc.edu>
2018-03-09 14:49:21 -0800

    NetworkManager with random auto join and custom properties to filter ingame rooms

--- Assets/Scripts/Network/NetworkManager.cs
+++ Assets/Scripts/Network/NetworkManager.cs
@@ -313,6 +313,11 @@ public class NetworkManager : PunBehaviour
         // The host will call the change scene
         if (PhotonNetwork.isMasterClient)
         {
+            // Set custom room property to distinguish the room is currently in game or not
+            ExitGames.Client.Photon.Hashtable roomInfo = new ExitGames.Client.Photon.Hashtable();
+            roomInfo.Add("InGame", false);
+            PhotonNetwork.room.SetCustomProperties(roomInfo);
+
             // Load new scene
             PhotonNetwork.LoadLevel(onlineSceneName);
         }
