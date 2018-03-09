21b069d49feef7b19829b95bacb4cadb764ce0e9
miguelmercado24 <32377727+miguelmercado24@users.noreply.github.com>
2018-03-09 14:09:10 -0800

    New_UI_First_Pass

--- Assets/Scripts/UIScripts/UIBoostBarControl.cs
+++ Assets/Scripts/UIScripts/UIBoostBarControl.cs
@@ -18,15 +18,12 @@ public class UIBoostBarControl : MonoBehaviour {
     void OnEnable()
     {
         progressBar = transform.Find("ProgressBar").gameObject.GetComponent<Image>();
-        percentage = transform.Find("Text").gameObject.GetComponent<Text>();
         bc = NetworkManager.localPlayer.GetComponent<BoostControl>();
     }
 
     void Update()
     {
         progressBar.fillAmount = bc.energy/bc.maxEnergy;
-
-        percentage.text = ""+(int)bc.energy / bc.maxEnergy*100+"%";
 
     }
 }
