53d4e0182eca955c30a438d23940946eb4e17d66
CarryRocky <32424253+CarryRocky@users.noreply.github.com>
2018-04-05 14:23:19 -0700

    Add new files

---
+++ Assets/Scripts/DamageSystem/Powerups/PunchingGlow/PunchingGlowControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Author: Carry
+ * 
+ * Description:
+ * Control class of Punching Glow.
+ */
+
+public class PunchingGlowControl : PowerUpBase
+{
+
+    // Use this for initialization
+    private void Awake()
+    {
+        this.enabled = photonView.isMine;
+    }
+
+    public override void AdjustModel()
+    {
+        base.AdjustModel();
+    }
+
+    protected override void OnPress()
+    {
+        
+    }
+}
