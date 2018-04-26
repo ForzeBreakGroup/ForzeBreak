0f431c2fe0b6e2a3859584aa3004b78838b178b1
Jason Lin <jlin116@ucsc.edu>
2018-04-26 16:19:14 -0700

    Network player name display

---
+++ Assets/Scripts/Network/PlayerNameDisplay.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerNameDisplay : MonoBehaviour
+{
+    TextMesh playerName;
+    Camera cam;
+
+    private void Awake()
+    {
+        playerName = GetComponent<TextMesh>();
+        cam = NetworkManager.instance.GetPlayerCamera();
+        PhotonView view = transform.root.gameObject.GetPhotonView();
+        PhotonPlayer p = PhotonPlayer.Find(view.ownerId);
+
+        playerName.text = p.NickName;
+        float[] serializeColor = PhotonPlayer.Find(view.ownerId).CustomProperties["Color"] as float[];
+        Color c = new Color(serializeColor[0], serializeColor[1], serializeColor[2], serializeColor[3]);
+        playerName.color = c;
+    }
+
+    private void Update()
+    {
+        Vector3 v = cam.transform.position - transform.position;
+        v.x = v.z = 0.0f;
+        transform.LookAt(cam.transform.position - v);
+        transform.Rotate(0, 180.0f, 0);
+    }
+}
