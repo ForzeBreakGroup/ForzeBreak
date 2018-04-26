b69faa7519e237ef15b720cdb7918c7f62b7c3b7
CarryRocky <32424253+CarryRocky@users.noreply.github.com>
2018-04-25 20:39:35 -0700

    powerupChanges

--- Assets/Scripts/DamageSystem/Powerups/Trap/BananaTrap/BananaMovement.cs
+++ Assets/Scripts/DamageSystem/Powerups/Trap/BananaTrap/BananaMovement.cs
@@ -3,24 +3,4 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BananaMovement : PowerUpForceMove {
-	public float ExistingTime = 10f;
-
-	private float spawnTime = 0f;
-
-	// Use this for initialization
-	protected override void Awake()
-	{
-		base.Awake ();
-		spawnTime = Time.time;
-	}
-
-	protected override void Move()
-	{
-		base.Move();
-
-		if (Time.time > spawnTime + ExistingTime)
-		{
-			PhotonNetwork.Destroy(gameObject);
-		}
-	}
 }
