253fb396ede09119f01bf2e70c33e026cab4f943
Jason Lin <jlin116@ucsc.edu>
2018-04-26 18:16:24 -0700

    KillCount

---
+++ Assets/Scripts/UIScripts/InGameUI/UIKillCountControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIKillCountControl : MonoBehaviour
+{
+    private Text killCount;
+
+    private void Awake()
+    {
+        killCount = GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        killCount.text = ((int)PhotonNetwork.player.CustomProperties["KillCount"]).ToString();
+    }
+}
