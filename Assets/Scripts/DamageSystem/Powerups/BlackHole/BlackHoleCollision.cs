5bd36f5c29e5e135d06954a84e10fdd586e3f3db
Jason Lin <jlin116@ucsc.edu>
2018-05-02 18:08:06 -0700

    Blackhole mechanic

--- Assets/Scripts/DamageSystem/Powerups/BlackHole/BlackHoleCollision.cs
+++ Assets/Scripts/DamageSystem/Powerups/BlackHole/BlackHoleCollision.cs
@@ -4,5 +4,45 @@ using UnityEngine;
 
 public class BlackHoleCollision : PowerUpCollision
 {
+    [SerializeField]
+    private float blackHoleRadius = 10;
 
+    [SerializeField]
+    private float blackHoleMaxPull = 10000f;
+
+    Collider blackHoleCollider;
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        blackHoleCollider = GetComponent(typeof(Collider)) as Collider;
+    }
+
+    protected override void CollisionEnter(Collision collision)
+    {
+        // Make the black hole as kinetic
+        rb.useGravity = false;
+        rb.isKinematic = true;
+
+        // Change the collider to trigger
+        blackHoleCollider.isTrigger = true;
+        ((SphereCollider)blackHoleCollider).radius = blackHoleRadius;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        // Blackhole ignores environment objects
+        if (other.tag != "Environment")
+        {
+            Rigidbody otherRb = other.transform.root.GetComponent<Rigidbody>();
+            // Apply constant pull force base on the distance between blackhole and gameobject
+            if (otherRb != null)
+            {
+                // Calculate Distance
+                float forceRatio = 1 - Vector3.Distance(transform.position, other.transform.position) / blackHoleRadius;
+                otherRb.AddForce((transform.position - otherRb.position).normalized * blackHoleMaxPull * forceRatio, ForceMode.Acceleration);
+            }
+        }
+    }
 }
--- Assets/Scripts/DamageSystem/Powerups/BlackHole/BlackHoleMovement.cs
+++ Assets/Scripts/DamageSystem/Powerups/BlackHole/BlackHoleMovement.cs
@@ -4,4 +4,11 @@ using UnityEngine;
 
 public class BlackHoleMovement : PowerUpMovement
 {
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * 15, ForceMode.Impulse);
+    }
 }
