1bed07640589ff43eb8b20beb491c4b710cd28e0
Jason Lin <jlin116@ucsc.edu>
2018-05-23 19:59:54 -0700

    Added setting menu and intro menu

---
+++ Assets/Scripts/UIScripts/MenuUI/MenuTransitionUIControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuTransitionUIControl : MonoBehaviour
+{
+    private enum MenuStates
+    {
+        Intro,
+        Main,
+        Setting
+    };
+    private MenuStates states = MenuStates.Intro;
+    private Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (states == MenuStates.Intro && Input.anyKey)
+        {
+            anim.SetTrigger("HideIntro");
+        }
+    }
+}
