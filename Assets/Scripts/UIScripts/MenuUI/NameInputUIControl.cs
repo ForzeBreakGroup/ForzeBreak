1e6a3fd4443179ab961d0d2b7005783139bcedfb
Jason Lin <jlin116@ucsc.edu>
2018-05-24 18:54:49 -0700

    player name input field

---
+++ Assets/Scripts/UIScripts/MenuUI/NameInputUIControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NameInputUIControl : MonoBehaviour
+{
+    private Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void OnConfirmNameChange()
+    {
+        // Change the photon player nickname
+
+        anim.SetTrigger("Hide");
+    }
+
+    public void OnCancelNameChange()
+    {
+        anim.SetTrigger("Show");
+    }
+}
---
+++ Assets/Scripts/UIScripts/MenuUI/PlayerNameSync.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerNameSync : MonoBehaviour
+{
+    Text playerNameDisplay;
+
+    private void Awake()
+    {
+        playerNameDisplay = GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        if (playerNameDisplay.text != NetworkManager.instance.playerName)
+        {
+            playerNameDisplay.text = NetworkManager.instance.playerName;
+        }
+    }
+}
