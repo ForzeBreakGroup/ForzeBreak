44d484f06e3e074659e7fcb8384211a51c3b44b5
Jason Lin <jlin116@ucsc.edu>
2018-05-30 01:28:15 -0700

    Added template for credits scene

---
+++ Assets/Scripts/UIScripts/CreditsUI/CreditsUIControl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsUIControl : MonoBehaviour
+{
+    [SerializeField]
+    private string returnSceneName;
+
+    public void OnClickReturn()
+    {
+        SceneManager.LoadScene(returnSceneName);
+    }
+}
