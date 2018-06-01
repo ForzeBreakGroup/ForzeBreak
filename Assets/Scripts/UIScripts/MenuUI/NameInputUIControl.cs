eef61885e09842d0660ad09bc2ffb0fd67495c6b
Jason Lin <jlin116@ucsc.edu>
2018-06-01 15:34:16 -0700

    Save player name preferences as well

--- Assets/Scripts/UIScripts/MenuUI/NameInputUIControl.cs
+++ Assets/Scripts/UIScripts/MenuUI/NameInputUIControl.cs
@@ -7,17 +7,21 @@ public class NameInputUIControl : MonoBehaviour
 {
     private Animator anim;
     private InputField inputfield;
+    private SettingDataControlInputField settingData;
 
     private void Awake()
     {
         anim = GetComponent<Animator>();
         inputfield = transform.Find("Panel").Find("InputField").GetComponent<InputField>();
+        settingData = GetComponent<SettingDataControlInputField>();
+        settingData.Load();
     }
 
     public void OnConfirmNameChange()
     {
         // Change the photon player nickname
         NetworkManager.instance.ChangePlayerName(inputfield.text);
+        settingData.Save();
 
         anim.SetTrigger("Hide");
     }
---
+++ Assets/Scripts/UIScripts/MenuUI/SettingDataControl/SettingDataControlInputField.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingDataControlInputField : SettingDataControlBase
+{
+    public override void Load()
+    {
+        if (PlayerPrefs.HasKey(settingDataKey))
+        {
+            NetworkManager.instance.ChangePlayerName(PlayerPrefs.GetString(settingDataKey));
+        }
+    }
+
+    public override void Save()
+    {
+        PlayerPrefs.SetString(settingDataKey, NetworkManager.instance.playerName);
+    }
+}
