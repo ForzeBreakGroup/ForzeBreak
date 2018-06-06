8976329304edafc7180c97e8e379f6f5c6590eb6
Jason Lin <jlin116@ucsc.edu>
2018-06-06 16:52:32 -0700

    Player Score board

---
+++ Assets/Scripts/UIScripts/InGameUI/UIPlayerScoreDisplay.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPlayerScoreDisplay : MonoBehaviour
+{
+    [SerializeField]
+    private int playerScoreIndex = 0;
+    private Text nameText;
+
+    private void Awake()
+    {
+        nameText = GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        PhotonPlayer p = UIPlayerScoreTracker.instance.GetPlayerAtPlace(playerScoreIndex);
+        if (p != null)
+        {
+            nameText.text = p.NickName + ": " + ((int)p.CustomProperties["KillCount"]).ToString();
+
+            float[] serializeColor = p.CustomProperties["Color"] as float[];
+            nameText.color = new Color(serializeColor[0], serializeColor[1], serializeColor[2], serializeColor[3]);
+        }
+    }
+}
---
+++ Assets/Scripts/UIScripts/InGameUI/UIPlayerScoreTracker.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIPlayerScoreTracker : MonoBehaviour
+{
+    private List<PhotonPlayer> playerScoreList;
+    private static UIPlayerScoreTracker playerScoreTracker;
+    public static UIPlayerScoreTracker instance
+    {
+        get
+        {
+            if (!playerScoreTracker)
+            {
+                playerScoreTracker = FindObjectOfType<UIPlayerScoreTracker>();
+                if (!playerScoreTracker)
+                {
+                    Debug.LogError("UIPlayerScoreTracker must be attached to an active gameobject in scene.");
+                }
+            }
+
+            return playerScoreTracker;
+        }
+    }
+
+    private void Awake()
+    {
+        playerScoreList = new List<PhotonPlayer>();
+
+        foreach (PhotonPlayer p in PhotonNetwork.playerList)
+        {
+            playerScoreList.Add(p);
+        }
+    }
+
+    private void Update()
+    {
+        int i = 0;
+        int j = 0;
+        for (i = 0; i < playerScoreList.Count - 1; i++)
+        {
+            int iKillCount = (int)playerScoreList[i].CustomProperties["KillCount"];
+            int max = i;
+
+            for (j = i + 1; j < playerScoreList.Count; j++)
+            {
+                int jKillCount = (int)playerScoreList[j].CustomProperties["KillCount"];
+                if (jKillCount > iKillCount)
+                {
+                    max = j;
+                }
+            }
+
+            if (max != i)
+            {
+                PhotonPlayer p = playerScoreList[i];
+                playerScoreList[i] = playerScoreList[max];
+                playerScoreList[max] = p;
+            }
+        }
+    }
+
+    public PhotonPlayer GetPlayerAtPlace(int i)
+    {
+        if (i >= playerScoreList.Count)
+        {
+            return null;
+        }
+        return playerScoreList[i];
+    }
+}
